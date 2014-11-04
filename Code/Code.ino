9dec77d9870b990b129baef2c12e1a22d0b490cf
higoraraujo <higor@live.com>
2014-11-04 01:13:34 -0300

    H-bridge - 6A
    Code - Controling one motor DC +  H-bridge 6A + Arduino  - by Igor
    Araujo 2014
    2x pins PWM for control.

---
+++ Code/Code.ino
@@ -0,0 +1,69 @@
+/*
+Code - Controling one motor DC +  H-bridge 6A + Arduino  - by Igor Araujo 2014
+2x pins PWM for control.
+
+0 = Low Speed
+100 = Full Speed
+
+https://github.com/higoraraujo/H-bridge-6A
+http://www.igoraraujo.eng.br
+
+*/
+
+
+
+
+#define INA 5
+#define INB 6
+
+void setup() {
+
+
+  pinMode(INA, OUTPUT);
+  pinMode(INB, OUTPUT);
+
+
+
+}
+
+void loop() {
+
+
+  motor(50); // 50% Speed forward
+  delay(2000);
+  motor(-50); // 50% Speed backward
+  delay(2000);
+
+}
+
+
+
+void motor (float a) {
+
+
+  if (a >= 0) {
+    a = (a * 2, 55);
+    analogWrite(INA, a);
+    analogWrite(INB, 0);
+
+  }
+  if (a < 0) {
+
+    a = a * (-2, 55);
+    analogWrite(INA, 0);
+    analogWrite(INB, a);
+
+
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
